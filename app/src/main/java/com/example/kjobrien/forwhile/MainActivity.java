9fe24736085b8321749f323e5011a715a32d9145
kevin <kevinjobrien420@gmail.com>
2018-06-06 03:40:01 -0500

    133 App Number 7 - Part1 - Master using For While and Switch Statements in Java //Complete

--- app/src/main/java/com/example/kjobrien/forwhile/MainActivity.java
+++ app/src/main/java/com/example/kjobrien/forwhile/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kjobrien.forwhile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtStart = (TextView)findViewById(R.id.txtStart);
+        Button btnLoop = (Button)findViewById(R.id.btnLoop);
+
+        btnLoop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                // -------------for LOOP----------
+                /* for (int i = 0; i <= 10; i++) {
+                    txtStart.setText(i + "");
+
+
+
+                // -------------WHILE LOOP--------------
+                int i = 0;
+
+                while (i <= 10) {
+
+                    txtStart.setText(i + "");
+                    i++;
+
+                }*/
+
+
+                // ------------ DO WHILE LOOP ------------
+                int y = 0;
+
+                do{
+                    txtStart.setText(y + "");
+                    y++;
+
+                } while (y <= 15);
+            }
+        });
     }
 }
